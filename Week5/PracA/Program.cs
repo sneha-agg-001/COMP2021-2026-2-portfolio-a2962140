1453c685213a4b3cf8c66d8617ddb5dcb2dc52cb
Sneha <sneha@student.adelaide.edu.au>
2026-09-01 12:40:18 +0930

    Complete Topic 5 Prac A Task 1

---
+++ Week5/PracA/Program.cs
@@ -0,0 +1,69 @@
+﻿string[] names =
+[
+    "Sneha",
+    "Aman",
+    "Priya",
+    "Christopher",
+    "Raj",
+    "Anjali",
+    "Michael",
+    "Simran",
+    "Alexander",
+    "Sam"
+];
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+string longest = names[0];
+string shortest = names[0];
+
+foreach (string name in names)
+{
+    if (name.Length > longest.Length)
+    {
+        longest = name;
+    }
+
+    if (name.Length < shortest.Length)
+    {
+        shortest = name;
+    }
+}
+
+Console.WriteLine("Longest name: " + longest);
+Console.WriteLine("Shortest name: " + shortest);
+
+Console.WriteLine("Before sorting:");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Array.Sort(names);
+
+Console.WriteLine("After sorting:");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine("Before reversing:");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
+
+Array.Reverse(names);
+
+Console.WriteLine("After reversing:");
+
+foreach (string name in names)
+{
+    Console.WriteLine(name);
+}
